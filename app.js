b44b3f8b80fddcb135e2afb777245d2129060d6a
celim1962 <43246276+celim1962@users.noreply.github.com>
2020-12-17 21:26:19 +0800

    adding file

---
+++ app.js
@@ -0,0 +1,19 @@
+new fullpage('#fullpage', {
+    autoScrolling: true,
+    navigation: true,
+    onLeave: (origin, destination, direction) => {
+        const section = destination.item;
+        const title = section.querySelector('h1');
+        const tl = new TimelineMax({ delay: 0.65 });
+        tl.fromTo(title, 1, { y: "10", opacity: 0 }, { y: 0, opacity: 1 });
+
+        if (destination.index === 1) {
+            const chair = document.querySelectorAll('.chair');
+            const description = document.querySelector('.description');
+            const tl = new TimelineMax({ delay: 1 });
+            tl.fromTo(description, 0.5, { x: "0", opacity: 0 }, { x: "40", opacity: 1 });
+            tl.fromTo(chair, 1, { x: "-495", opacity: 0 }, { x: "-500", opacity: 1 });
+
+        }
+    }
+});
